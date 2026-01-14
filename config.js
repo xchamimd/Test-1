6a1abbe575b0299fe5e42aef292088fe5acdc16c
Chamidu Harshana <xchamimd@gmail.com>
2026-01-14 23:25:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
 // මෙතන ඔයාගේ අලුත් Image Link එක දාන්න
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/xchamimd/Test-1/blob/ec318cdbe6371b689b7e8c867e7f8b358e09b73a/images/alive.png?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 xCHAMi MD Is Alive Now😍*",
-BOT_OWNER: '94776121326', // ඔයාගේ නම්බර් එක මෙතන දාන්න
+BOT_OWNER: '94701771975', // ඔයාගේ නම්බර් එක මෙතන දාන්න
 };
